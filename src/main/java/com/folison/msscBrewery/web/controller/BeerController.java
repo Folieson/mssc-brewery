dfa54490fc8efd444b58c2742a54910d64b513e7
Andrey Ponamarchuk <andrey.ponamarchuk@x5.ru>
2021-08-30 16:58:26 +0500

    add MVC Controller Advice

--- src/main/java/com/folison/msscBrewery/web/controller/BeerController.java
+++ src/main/java/com/folison/msscBrewery/web/controller/BeerController.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,10 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.ConstraintViolationException;
 import javax.validation.Valid;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
 @RequestMapping("/api/v1/beer")
@@ -53,13 +49,5 @@ public class BeerController {
   @ResponseStatus(HttpStatus.NO_CONTENT)
   public void deleteBeer(@PathVariable("beerId") UUID beerId) {
     beerService.deleteBeerById(beerId);
-  }
-
-  @ExceptionHandler(ConstraintViolationException.class)
-  public ResponseEntity<List<String>> validationErrorHandler(ConstraintViolationException exception) {
-    List<String> errors = new ArrayList<>(exception.getConstraintViolations().size());
-    exception.getConstraintViolations().forEach(constraintViolation ->
-        errors.add(constraintViolation.getPropertyPath() + " : " + constraintViolation.getMessage()));
-    return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
   }
 }
--- src/main/java/com/folison/msscBrewery/web/controller/CustomerController.java
+++ src/main/java/com/folison/msscBrewery/web/controller/CustomerController.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,10 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.ConstraintViolationException;
 import javax.validation.Valid;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
 @RequestMapping("/api/v1/customer")
@@ -54,13 +50,5 @@ public class CustomerController {
   @ResponseStatus(HttpStatus.NO_CONTENT)
   public void deleteCustomer(@PathVariable("customerId") UUID customerId) {
     customerService.deleteCustomerById(customerId);
-  }
-
-  @ExceptionHandler(ConstraintViolationException.class)
-  public ResponseEntity<List<String>> validationErrorHandler(ConstraintViolationException exception) {
-    List<String> errors = new ArrayList<>(exception.getConstraintViolations().size());
-    exception.getConstraintViolations().forEach(constraintViolation ->
-        errors.add(constraintViolation.getPropertyPath() + " : " + constraintViolation.getMessage()));
-    return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
   }
 }
